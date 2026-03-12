3138ba4024abc9693f20fee4ebfe30526d6f9685
degraffbh <benjamindegraff@gmail.com>
2026-03-12 17:30:52 -0600

    Quick about me update to clarify to users that only songs of 5 minutes or less can be searched and played

--- src/about/about.jsx
+++ src/about/about.jsx
@@ -6,7 +6,7 @@ export function About() {
   return (
     <main>
       <h2>About Vibe Check</h2>
-        <p>Vibe Check is a full-stack project made by Ben DeGraff, a CS Student at BYU. Vibe Check was created in a period of around 4 months and furthur development is TBD.</p>
+        <p>Vibe Check is a full-stack project made by Ben DeGraff, a CS Student at BYU. Vibe Check was created in a period of around 4 months and furthur development is TBD. All songs played are 5 minutes or less.</p>
         <h3>Vibe Check's Goal</h3>
         <p>Vibe Check aims to allow users to listen to music with their friends and family in a fair and shared way. Users can all suggest songs, chat with one another, and listen together. The experience is made to be shared, and enjoying music together is something that we believe is very special.</p>
         <h3>Bugs Or Errors?</h3>
