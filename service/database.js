1148ee7074b29c0bcad31fe9a732a63301a0c101
degraffbh <benjamindegraff@gmail.com>
2026-03-22 18:41:23 -0600

    Database.js logic mostly complete

--- service/database.js
+++ service/database.js
@@ -19,3 +19,101 @@ const queueCollection = db.collection('queue');
     process.exit(1);
   }
 })();
+
+function getSongLookupCriteria(songOrQuery) {
+  if (!songOrQuery) {
+    return {};
+  }
+
+  if (typeof songOrQuery === 'string') {
+    return { id: songOrQuery };
+  }
+
+  if (songOrQuery._id) {
+    return { _id: songOrQuery._id };
+  }
+
+  if (songOrQuery.id) {
+    return { id: songOrQuery.id };
+  }
+
+  if (songOrQuery.videoId && songOrQuery.user) {
+    return {
+      videoId: songOrQuery.videoId,
+      user: songOrQuery.user,
+    };
+  }
+
+  if (songOrQuery.videoId) {
+    return { videoId: songOrQuery.videoId };
+  }
+
+  if (songOrQuery.title && songOrQuery.user) {
+    return {
+      title: songOrQuery.title,
+      user: songOrQuery.user,
+    };
+  }
+
+  if (songOrQuery.name) {
+    return { name: songOrQuery.name };
+  }
+
+  return songOrQuery;
+}
+
+function getUser(email) {
+  return userCollection.findOne({ email: email });
+}
+
+function getUserByToken(token) {
+  return userCollection.findOne({ token: token });
+}
+
+async function addUser(user) {
+  await userCollection.insertOne(user);
+}
+
+async function updateUser(user) {
+  await userCollection.updateOne({ email: user.email }, { $set: user });
+}
+
+async function updateUserRemoveAuth(user) {
+  await userCollection.updateOne({ email: user.email }, { $unset: { token: 1 } });
+}
+
+async function getQueue(queue) {
+  if (!queue) {
+    return queueCollection.find({}).toArray();
+  }
+
+  return queueCollection.findOne(getSongLookupCriteria(queue));
+}
+
+async function addSongToQueue(song) {
+  if (!song || typeof song !== 'object') {
+    throw new Error('A song object is required');
+  }
+
+  await queueCollection.insertOne({ ...song });
+}
+
+async function deleteSongFromQueue(song) {
+  const criteria = getSongLookupCriteria(song);
+  if (Object.keys(criteria).length === 0) {
+    throw new Error('A song id or song object is required');
+  }
+
+  await queueCollection.deleteOne(criteria);
+}
+
+module.exports = {
+  getUser,
+  getUserByToken,
+  addUser,
+  updateUser,
+  updateUserRemoveAuth,
+  getQueue,
+  addSongToQueue,
+  deleteSongFromQueue
+};
